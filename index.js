42beb743cd32d72b6a10ca222311434db61e4f1d
Sayaka.K <saya.9no@gmail.com>
2021-08-14 23:16:09 +0900

    front-page

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ $(function(){
   $('.js-btn').on('click',function(){
     $('.l-aside').toggleClass('is-open');
     $('.l-blackLayer').toggleClass('is-open');
+    $('.p-global-nav').toggleClass('is-open');
+    $('.c-button--nav').toggleClass('is-open');
   });
 });
